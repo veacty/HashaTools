776330918f7672ee7acee6d6bd10301831b5f332
Kacper <happi@memeware.net>
2018-04-30 16:31:20 +0200

    code cleanup

--- hashatools-api/src/main/java/com/moizdrajcy/toolsapi/kit/KitManager.java
+++
@@ -1,5 +0,0 @@
-package com.moizdrajcy.toolsapi.kit;
-
-public interface KitManager {
-
-}
--- hashatools-bukkit/src/main/java/com/moizdrajcy/tools/kit/KitManagerImpl.java
+++
@@ -1,7 +0,0 @@
-package com.moizdrajcy.tools.kit;
-
-import com.moizdrajcy.toolsapi.kit.KitManager;
-
-public class KitManagerImpl implements KitManager {
-
-}
